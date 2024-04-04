98c73f54c70eada3f2dac30bfe29cf586d40518f
Ekaterina Petukhova <katepthv12@gmail.com>
2024-04-04 23:39:51 +0200

    feat: solve getArgumentsCount

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -50,8 +50,12 @@ function getFunctionBody(func) {
  *  ]) => [0, 1, 2]
  *
  */
-function getArgumentsCount(/* funcs */) {
-  throw new Error('Not implemented');
+function getArgumentsCount(funcs) {
+  const argumentsCount = [];
+
+  funcs.forEach((func) => argumentsCount.push(func.length));
+
+  return argumentsCount;
 }
 
 /**
