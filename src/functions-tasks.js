425c64bbd61079f20fca822a3315b566e5b4ead9
Ekaterina Petukhova <katepthv12@gmail.com>
2024-04-06 18:58:06 +0200

    feaf: solve logger

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -195,8 +195,17 @@ function retry(func, attempts) {
  * cos(3.141592653589793) ends
  *
  */
-function logger(/* func, logFunc */) {
-  throw new Error('Not implemented');
+function logger(func, logFunc) {
+  return (...args) => {
+    const params = args.map((arg) => JSON.stringify(arg)).join(',');
+    const start = `${func.name}(${params}) starts`;
+    const end = `${func.name}(${params}) ends`;
+    logFunc(start);
+    const result = func(...args);
+    logFunc(end);
+
+    return result;
+  };
 }
 
 /**
